6f3e7b05fefb32bfbabd572b5780a95b3bcd3a14
BelovitskiyDA <89989289+BelovitskiyDA@users.noreply.github.com>
2021-09-09 18:37:06 +0300

    menu template

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,7 +1,70 @@
 #include <iostream>
 
+using namespace std;
+
+void printMenu()
+{
+	cout << "[1] Add pipe" << endl
+		<< "[2] Add CS" << endl
+		<< "[3] View all objects" << endl
+		<< "[4] Edit pipe" << endl
+		<< "[5] Edit CS" << endl
+		<< "[6] Save" << endl
+		<< "[7] Load" << endl
+		<< "[0] Exit" << endl;
+}
+
 int main()
 {
-
+	while (true)
+	{
+		printMenu();
+		int i = 0;
+		cin >> i;
+		switch (i)
+		{
+		case 1:
+		{
+			//addPipe();
+			break;
+		}
+		case 2:
+		{
+			//addCS();
+			break;
+		}
+		case 3:
+		{
+			//viewAllObjects();
+			break;
+		}
+		case 4:
+		{
+			//editPipe();
+			break;
+		}
+		case 5:
+		{
+			//editCS();
+			break;
+		}
+		case 6:
+		{
+			//Save();
+			break;
+		}
+		case 7:
+		{
+			//Load();
+			break;
+		}
+		case 0:
+		{
+			return 0;
+		}
+		default:
+			cout << "Wrong action" << endl;
+		}
+	}
 	return 0;
 }
